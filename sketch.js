a7989b166d00454fc6f00896eb4b67fe423700ce
Andreas Tagger <atagger1@gmail.com>
2018-06-11 19:48:07 -0700

    New
    New prototype with two streams and JSON loading more robustly.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,30 @@
 //array for the tile objects
 var tiles = [];
+var tiles2 = [];
 
 //array for the image data
 var imageTiles = [];
+var imageTiles2 = [];
+
 var logo;
+var data;
+var xoff=0;
+var textWidth;
 
 //preload the image data
 function preload() {
 
-  for (var i = 0; i < 5; i++) {
-    imageTiles[i] = loadImage('images/image' + i + '.jpg')
-  }
+//  for (var i = 0; i < 5; i++) {
+  //  imageTiles[i] = loadImage('images/image' + i + '.jpg')
+//  }
 
 logo = loadImage('images/o.png')
+
+//call gotData function after preload
+data = loadJSON("data.json", gotData);
+
+//load font
+//myFont = loadFont('font/europa-regular.otf');
 
 }
 
@@ -29,18 +41,17 @@ function setup() {
   //createCanvas(500, 500);
   background(255);
 
-  loadJSON("data.json",gotData);
+  textWidth=100;
+//  loadJSON("dataxx.json",gotData);
 
-  for (var i = 0; i < 60; i++) {
+  //initialize the first stream
+  for (var i = 0; i < 20; i++) {
 
-    //set random speeds
-   //  var speed = random(.1,3);
-
-   // set random sizes
-   // var size=random(10,70);
-
+    var picker = floor(random(0,imageTiles.length));
     //select randomly from image array
-    var img=imageTiles[floor(random(0,5))];
+    var img=imageTiles[picker][0];
+    var title=imageTiles[picker][1];
+    var author=imageTiles[picker][2];
     //scale width randomly
     var width=img.width/random(5,10);
     //calculate aspect ratio of image
@@ -48,45 +59,132 @@ function setup() {
     //change height to aspect ratio so image is scaled uniformly
     var height=width*aspectRatio;
 
-    var x = random(windowHeight-height);
+    var x = random(0,(windowHeight/2)-height);
 
     var position = createVector(random(windowWidth), x);
-    var velocity = createVector(random(.5),0);
+    var velocity = createVector(random(.25),0);
     var acceleration = createVector(0,0);
     var mass = random(aspectRatio*8,aspectRatio*16);
 
     //var force = createVector(.2,0);
 
-    //construct a tile
-    tiles[i] = new contentTile(position.x, position.y, width, height, img, position, velocity, acceleration, mass);
+    //construct tiles
+    tiles[i] = new contentTile(position.x, position.y, width, height, img, position, velocity, acceleration, mass, title, author);
+  }
+
+  //initialize a second stream
+  for (var i = 0; i < 30; i++) {
+
+    var picker = floor(random(0,imageTiles2.length));
+    //select randomly from image array
+    var img=imageTiles2[picker][0];
+    var title=imageTiles2[picker][1];
+    var author=imageTiles2[picker][2];
+
+    //scale width randomly
+    var width=img.width/random(5,10);
+    //calculate aspect ratio of image
+    var aspectRatio = img.height/img.width;
+    //change height to aspect ratio so image is scaled uniformly
+    var height=width*aspectRatio;
+
+    var x = random(windowHeight/2,windowHeight-height);
+
+    var position = createVector(random(windowWidth), x);
+    var velocity = createVector(random(-.25),0);
+    var acceleration = createVector(0,0);
+    var mass = random(aspectRatio*8,aspectRatio*16);
+
+    //var force = createVector(.2,0);
+
+    //construct tiles
+    tiles2[i] = new contentTile(position.x, position.y, width, height, img, position, velocity, acceleration, mass, title, author);
   }
 
 }
 
 //do stuff when we get the json data
 function gotData(data) {
-console.log(data);
-console.log(data[0]);
-console.log(data[0].blue);
-console.log(data[0].red);
+
+console.log("got the JSON data, over and out commander!");
+console.log(data[0].tag);
+//console.log(data);
+//console.log(data[0]);
+//console.log(data[0].OneonOne.articles[0].img);
+
+
+//load the images into the tile array - stream 1
+for (var i = 0; i < data[0].articles.length; i++) {
+  //define a 2d array for the data
+  imageTiles[i]=[];
+  imageTiles[i][0] = loadImage(data[0].articles[i].img)
+  imageTiles[i][1] = data[0].articles[i].title;
+  imageTiles[i][2] = data[0].articles[i].author;
+
+}
+
+for (var i = 0; i < data[1].articles.length; i++) {
+  //define a 2d array for the data
+  imageTiles2[i]=[];
+  imageTiles2[i][0] = loadImage(data[1].articles[i].img)
+  imageTiles2[i][1] = data[1].articles[i].title;
+  imageTiles2[i][2] = data[1].articles[i].author;
+//  imageTiles.splice(imageTiles.length,0,loadImage(data[0].OneonOne.articles[i].img));
+}
 
 }
 
 function draw() {
   // put drawing code here
   background(255);
-  //rectMode(CENTER);
+
   for (var i = 0; i < tiles.length; i++) {
   //  var gravity = createVector(4,0.2*tiles[i].mass);
 
-    var movingForce = createVector(.025,0);
+    //perlin noise
+  //  xoff = xoff + 0.03;
+  //  var n = map(noise(xoff), 0, 1, -.01,.01);
+  //  console.log(n)
+
+    var movingForce = createVector(.01,0);
     tiles[i].applyForce(movingForce);
 
     tiles[i].move(mouseX,mouseY,tiles[i]);
     tiles[i].display();
     tiles[i].checkEdges();
+  //  tint(255,127);
+
   }
 
+  for (var i = 0; i < tiles2.length; i++) {
+  //  var gravity = createVector(4,0.2*tiles[i].mass);
+
+    //perlin noise
+  //  xoff = xoff + 0.03;
+  //  var n = map(noise(xoff), 0, 1, -.01,.01);
+  //  console.log(n)
+
+    var movingForce = createVector(-.01,0);
+    tiles2[i].applyForce(movingForce);
+
+    tiles2[i].move(mouseX,mouseY,tiles[i]);
+    tiles2[i].display();
+    tiles2[i].checkEdges();
+  //  tint(255,127);
+
+  }
+
+  //display open space logo
+  //  tint(255,127);
+  image(logo,(windowWidth/2)-200,(windowHeight/2)-200,400,400);
+
+  var fps = frameRate();
+  textSize(12);
+  fill(0,0,255);
+  text("FPS: " + fps.toFixed(2), 10, 20);
+
+//  noTint();
+//xoff=0;
 }
 
 function mousePressed() {
@@ -100,12 +198,24 @@ function mousePressed() {
   }
 
     //if not dragging and clicking white space blow wind
-    var wind = createVector(10,random(15));
+    var wind = createVector(-10,0);
     tiles[i].applyForce(wind);
 
-
-
  }
+
+ for (var i = tiles2.length-1; i >= 0; i--) {
+   tiles2[i].clicked(mouseX,mouseY,i);
+
+ //if dragging exit the loop so multiple tiles are not selected
+ if(tiles2[i].dragging) {
+   break;
+ }
+
+   //if not dragging and clicking white space blow wind
+   var wind = createVector(5,0);
+   tiles2[i].applyForce(wind);
+
+}
 
 }
 
@@ -113,12 +223,17 @@ function mouseReleased() {
   for (var i = 0; i < tiles.length; i++) {
       tiles[i].dragging=false;
  }
+
+ for (var i = 0; i < tiles2.length; i++) {
+     tiles2[i].dragging=false;
+}
+
 }
 
 class contentTile {
 
   //the equals here is when no values are passed to the constructor, it defaults to these
-  constructor(x=0,y=0,width=25,height=25,imageSource='image0.jpg', position, velocity, acceleration, mass) {
+  constructor(x=0,y=0,width=25,height=25,imageSource='image0.jpg', position, velocity, acceleration, mass, title, author) {
 
     //set up constructor with values
     this.x=x;
@@ -130,6 +245,8 @@ class contentTile {
     this.velocity=velocity;
     this.acceleration=acceleration;
     this.mass=mass;
+    this.title=title;
+    this.author=author;
   }
 
 
@@ -187,9 +304,9 @@ class contentTile {
     if(this.position.x >= windowWidth) {
 
       this.velocity.x*=.5;
-      this.position.x=0-this.width;
+      this.position.x=0-this.width - textWidth;
 
-    } else if(this.position.x < 0-this.width) {
+    } else if(this.position.x < 0-(this.width+textWidth)) {
       this.velocity.x*=.5;
       this.position.x=windowWidth;
     }
@@ -209,11 +326,19 @@ class contentTile {
 
 
   display() {
-    image(logo,(windowWidth/2)-200,(windowHeight/2)-200,400,400);
-    //display the images
-    image(this.imageSource,this.position.x,this.position.y,this.width,this.height)
-   //  rect(this.x, this.y, this.width, this.height);
 
+   //display the images
+   image(this.imageSource,this.position.x,this.position.y,this.width,this.height)
+   //  rect(this.x, this.y, this.width, this.height);
+   textSize(12);
+   fill(0,0,0);
+   textFont("Georgia");
+   //  noStroke();
+   //  rect(this.position.x+this.width+5,this.position.y,105,105)
+
+   text(this.title + " by " + this.author, this.position.x+this.width+10, this.position.y,textWidth,150);
+   // textSize(9);
+   // text('by '+ this.author, this.position.x, this.position.y+this.height+5,70,20);
   }
 
 }
